12997b3e766d3e72bbd3eeacf7e40b1116f810a9
Konstantin8105 <konstantin8105@gmail.com>
2018-02-27 16:50:04 +0300

    refactoring

--- pi.go
+++ pi.go
@@ -35,7 +35,7 @@ var (
 )
 
 // calculate next increment
-func (s *PiService) calc(den big.Int, cIncrement chan<- *big.Float) {
+func (s *PiService) calculate(den big.Int, cIncrement chan<- *big.Float) {
 	var next big.Float
 	next.SetInt(&den)
 	next.Quo(one, &next)
@@ -75,14 +75,12 @@ func (s *PiService) Start() {
 				}
 
 				// calculation
-				s.calc(den, cIncrement)
-
+				s.calculate(den, cIncrement)
 				// prepare for next iteration
 				s.prepare(&den, &minus)
 
 				s.iter.Add(s.iter, oneInt)
 			}
-
 		}()
 
 		// add increment to result
